6800db0110f6f8806b108d2cc31b447aed80f6ad
Alireza Andersen <alireza.andersen@gmail.com>
2016-04-28 21:07:55 -0600

    created model testing for student and teacher

--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_student_can_be_created
+    ali = Student.new(first_name: "Ali", last_name: "Andersen", age: "30", current_score: "90")
+    assert ali.save
+  end
 end
--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class TeacherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def test_teacher_can_be_created
+    nate = Teacher.new(first_name: "Nate", last_name: "Allen")
+    assert nate.save
+  end
+
+  def test_can_create_a_student_through_teacher
+    nate = Teacher.new(first_name: "Nate", last_name: "Allen")
+    assert nate.save
+    assert nate.students.create(first_name: "Ali", last_name: "Andersen", age: "35")
+  end
 end
